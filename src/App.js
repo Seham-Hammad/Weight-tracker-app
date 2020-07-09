129bbd8a9ce53133145d1141725a08ee081857a2
7101981 <43120539+7101981@users.noreply.github.com>
2020-07-09 18:14:44 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import image from './images/image.jpg';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import Measures from './Components/Measures';
+import AddMeasures from './Components/AddMeasures';
+import Home from './Components/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="app-header">
+          <ul className="ul-style">
+            <li className="li-style">
+              <NavLink to="/" className="App-link">
+                <img className="card--image" src={image} alt='' />
+              </NavLink>
+            </li>
+            <li className="li-style">
+              <NavLink to="/measuresDetails" className="App-link">
+                Measures Detials
+              </NavLink>
+            </li>
+            <li className="li-style">
+              <NavLink to="/addMeasures" className="App-link">
+                Add Measures
+              </NavLink>
+            </li>
+          </ul>
+
+          <Route path='/' exact component={Home} />
+          <Route path='/measuresDetails' component={Measures} />
+          <Route path='/addMeasures' component={AddMeasures} />
+
+        </header>
+      </div>
+    </Router>
   );
 }
 
